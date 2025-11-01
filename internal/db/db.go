d072abc6a7d12508f5bffc61c3676d68a04d27ff
Antonious Stewart <antonious.stewart@gmail.com>
2025-10-31 20:21:59 -0400

    Add Dockerfile and db Pool

---
+++ internal/db/db.go
@@ -0,0 +1,45 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/Antonious-Stewart/Aggregator/internal/config"
+	"log"
+)
+
+var Pool *sql.DB
+
+func logDBErrorValues(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func init() {
+	host, err := config.GetVar("DB_HOST")
+	logDBErrorValues(err)
+
+	port, err := config.GetVar("DB_PORT")
+	logDBErrorValues(err)
+
+	user, err := config.GetVar("DB_USER")
+	logDBErrorValues(err)
+
+	password, err := config.GetVar("DB_PASSWORD")
+	logDBErrorValues(err)
+
+	name, err := config.GetVar("DB_NAME")
+	logDBErrorValues(err)
+
+	sslmode, err := config.GetVar("SSLMODE")
+	logDBErrorValues(err)
+
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, name, sslmode)
+	db, err := sql.Open("postgres", connectionString)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Pool = db
+}
